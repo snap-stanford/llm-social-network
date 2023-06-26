114bc0612ee27b175ac2ac7c1fd0eebceb1b2cda
Serina Chang <serinac@stanford.edu>
2023-06-26 14:38:20 -0400

    updated demo code with folders

--- demo.py
+++ demo.py
@@ -1,23 +1,33 @@
-import networkx as nx
-import re
 import matplotlib.pyplot as plt
+import networkx as nx
+import os
 
-def parse_network_from_gpt_output(node_fn, edge_fn, drop_isolates=True, draw=True):
+PATH_TO_TEXT_FILES = './text-files'  # folder holding text files, typically GPT output
+PATH_TO_SAVED_PLOTS = './saved-plots'  # folder holding plots, eg, network figures
+
+def parse_network_from_gpt_output(node_fn, edge_fn, 
+                                  drop_isolates=True, draw_network=True):
     """
-    Parse generated network from ChatGPT.
+    Parse generated network from GPT.
     Args:
         node_fn: filename of node list (list of personas). Example: nodes.txt
         edge_fn: filename of edge list. Example: network1.txt
+        drop_isolates: whether to drop nodes without connections in network.
+        draw_network: whether to draw network (figure is automatically saved).
     """
     G = nx.Graph()
     # get nodes
-    with open(node_fn, 'r') as f:
+    node_path = os.path.join(PATH_TO_TEXT_FILES, node_fn)
+    assert os.path.isfile(node_path)  # check that expected file exists
+    with open(node_path, 'r') as f:
         lines = f.readlines()
         for l in lines:
             node = l.split(',', 1)[0]
             G.add_node(node)
     # get edges
-    with open(edge_fn, 'r') as f:
+    edge_path = os.path.join(PATH_TO_TEXT_FILES, edge_fn)
+    assert os.path.isfile(edge_path)  # check that expected file exists
+    with open(edge_path, 'r') as f:
         lines = f.readlines()
         for l in lines:            
             pair = l.split(')', 1)[0]
@@ -33,13 +43,22 @@ def parse_network_from_gpt_output(node_fn, edge_fn, drop_isolates=True, draw=Tru
         for i in isolates:
             print(i)
         G.remove_nodes_from(isolates)
-    # draw graph
-    if draw:
-        nx.draw_networkx(G)
+    # draw network
+    if draw_network:
+        nx.draw_networkx(G, pos=nx.spring_layout(G, seed=0))
         plt.axis("off")  # turn off axis
-        plt.show()
+        axis = plt.gca()
+        axis.set_xlim([1.1*x for x in axis.get_xlim()])  # add padding so that node labels aren't cut off
+        axis.set_ylim([1.1*y for y in axis.get_ylim()])
+        plt.tight_layout()
+
+        fig_fn = edge_fn.split('.', 1)[0] + '.png'  # get filename without extension
+        fig_path = os.path.join(PATH_TO_SAVED_PLOTS, fig_fn)
+        print('Saved network drawing in ', fig_path)
+        plt.savefig(fig_path)
     return G     
 
 
 if __name__ == '__main__':
-    parse_network_from_gpt_output('nodes.txt', 'network1.txt')  # could change this to read from command line
+    # could change this to read from command line
+    parse_network_from_gpt_output('nodes.txt', 'network3.txt')  
