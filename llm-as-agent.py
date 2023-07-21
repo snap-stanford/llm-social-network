1737c6779bcf5d5315eac5cc399d439366c04512
Serina Chang <serinac@stanford.edu>
2023-07-21 16:18:44 -0700

    change personas and demo_keys to arguments instead of global variables

--- llm-as-agent.py
+++ llm-as-agent.py
@@ -5,27 +5,19 @@ import networkx as nx
 import time
 import matplotlib.pyplot as plt
 from constants_and_utils import *
+from generatepersonas import *
 
-def generate_string_with_demos(name, demos_to_include):
+def generate_prompt_for_person(name, personas, demo_keys, demos_to_include='all', perspective='second'):
     """
-    Generate string for person, specifying which demographics to include (if any).
-    """
-    demo_vals = PERSONAS[name].split(', ')
-    demo2val = dict(zip(DEMO_KEYS, demo_vals))
-    s = name
-    if len(demos_to_include) > 0:
-        s += ' - '
-        demo_vals_to_include = [demo2val[d] if d != 'age' else f'age {demo2val[d]}' for d in demos_to_include]
-        s += ', '.join(demo_vals_to_include)
-    return s
-
-def generate_prompt_for_person(name, perspective='second', demos_to_include=DEMO_KEYS):
-    """
-    Generate prompt for person.
+    Generate LLM-as-agent prompt for persona.
+    personas: dict of name (str) : demographics (list)
+    demo_keys: which demographics are represented in list (eg, gender, age)
+    name: name of person to generate prompt for
+    demo_to_include: which demographics to include in prompt
+    perspective: which perspective (first, second, or third) to phrase prompt
     """
     assert perspective in {'first', 'second', 'third'}, f'Not a valid perspective: {perspective}'
-    assert name in PERSONAS
-    person_str = generate_string_with_demos(name, demos_to_include)
+    person_str = convert_persona_to_string(name, personas, demo_keys, demos_to_include)
     if perspective == 'first':  # first person
         prompt = f'I am {person_str}. Which of the following people will I become friends with?\n'
         friends_prefix = 'My friends:'
@@ -36,32 +28,15 @@ def generate_prompt_for_person(name, perspective='second', demos_to_include=DEMO
         prompt = f'This is {person_str}. Which of the following people will {name} become friends with?\n'
         friends_prefix = 'Friends'
 
-    for n in PERSONAS:
+    for n in personas:
         if name != n:
-            prompt += generate_string_with_demos(n, demos_to_include) + '\n'
+            prompt += convert_persona_to_string(n, personas, demo_keys, demos_to_include) + '\n'
     prompt += f'{friends_prefix}:\n1.'  # begin numbered list
     return prompt
 
-def get_gpt_output_for_person(name, prompt_kwargs):
-    """
-    Issue API call for person, check API response.
-    """
-    prompt = generate_prompt_for_person(name, **prompt_kwargs)
-    response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "system", "content": prompt}
-                        ],
-                    temperature=0.6)
-    response = response.choices[0]
-    finish_reason = response['finish_reason']
-    if finish_reason != 'stop':
-        raise Exception(f'Response for {name} stopped for reason {finish_reason}')
-    return response['message']['content']
-
 def get_new_edges_from_gpt_output(out, source_node, valid_nodes):
     """
-    Convert GPT output to new edges.
+    Convert GPT output to new directed edges.
     """
     new_edges = []
     lines = out.split('\n')
@@ -73,19 +48,24 @@ def get_new_edges_from_gpt_output(out, source_node, valid_nodes):
         new_edges.append((source_node, target_node))
     return new_edges
 
-def construct_network(max_tries=10, save_prefix=None, prompt_kwargs={}):
+def construct_network(personas, demo_keys, max_tries=10, save_prefix=None, prompt_kwargs={}):
     """
     Iterate through personas, issue API call, construct network.
     """
     G = nx.DiGraph()
-    G.add_nodes_from([get_node_from_string(n) for n in PERSONAS])
+    G.add_nodes_from([get_node_from_string(n) for n in personas])
     # iterate through personas
-    for name, demo in PERSONAS.items():
+    for name, demo in personas.items():
         print(f'{name} - {demo}')
         for t in range(max_tries):
             try:
                 print(f'Attempt #{t}')
-                out = get_gpt_output_for_person(name, prompt_kwargs) 
+                prompt = generate_prompt_for_person(name, personas, demo_keys, **prompt_kwargs)
+                response = openai.ChatCompletion.create(
+                                model="gpt-3.5-turbo",
+                                messages=[{"role": "system", "content": prompt}],
+                                temperature=DEFAULT_TEMPERATURE)
+                out = extract_gpt_output(response)
                 print(out)
                 new_edges = get_new_edges_from_gpt_output(out, get_node_from_string(name), list(G.nodes()))
                 # only update the graph for this person once every function passed
@@ -101,12 +81,26 @@ def construct_network(max_tries=10, save_prefix=None, prompt_kwargs={}):
 
 if __name__ == "__main__":
     # Example call: 
-    # nohup python3 -u llm-as-agent.py 60 > second-person-60-v2.out 2>&1 & 
+    # nohup python3 -u llm-as-agent.py personas_30.txt --save_prefix second-person-n30-1 > second-person-n30-1.out 2>&1 & 
     parser = argparse.ArgumentParser()
-    parser.add_argument('seed', type=int)
+    parser.add_argument('persona_fn', type=str)
+    parser.add_argument('--save_prefix', type=str, default='')
     parser.add_argument('--perspective', type=str, choices=['first', 'second', 'third'], default='second')
+    parser.add_argument('--demos_to_include', type=str, default='all')
     args = parser.parse_args()
 
-    prefix = f'{args.perspective}-person-{args.seed}'
-    print('Prefix:', prefix)
-    G = construct_network(save_prefix=prefix, prompt_kwargs={'perspective': args.perspective})
+    fn = os.path.join(PATH_TO_TEXT_FILES, args.persona_fn)
+    personas, demo_keys = load_personas_as_dict(fn, verbose=False)
+    save_prefix = args.save_prefix if len(args.save_prefix) > 0 else None
+    demos_to_include = args.demos_to_include if args.demos_to_include == 'all' else args.demos_to_include.split(',')
+
+    # check prompt
+    # test_name = list(personas.keys())[0]
+    # prompt = generate_prompt_for_person(test_name, personas, demo_keys, demos_to_include=demos_to_include, 
+    #                                     perspective=args.perspective)
+    # print(prompt)
+    
+    # construct network
+    prompt_kwargs = {'perspective': args.perspective,
+                     'demos_to_include': demos_to_include}
+    G = construct_network(personas, demo_keys, save_prefix=save_prefix, prompt_kwargs=prompt_kwargs)
