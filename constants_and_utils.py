71fcabd47f261b24b801d8b02013180fa0efcb6a
Serina Chang <serinac@stanford.edu>
2023-07-21 16:27:12 -0700

    removed personas and demo_keys as global variables; added util function

--- constants_and_utils.py
+++ constants_and_utils.py
@@ -1,31 +1,19 @@
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
 import os
 import openai
 
 PATH_TO_TEXT_FILES = './text-files'  # folder holding text files, typically GPT output
 PATH_TO_SAVED_PLOTS = './saved-plots'  # folder holding plots, eg, network figures
-PERSONAS = {'Emma Thompson': 'Woman, 30, White, Independent',
-            'Malik Johnson': 'Man, 45, Black, Liberal',
-            'Sofia Rodriguez': 'Woman, 22, Latino, Moderate',
-            'Ryan Chen': 'Man, 28, Asian, Conservative',
-            'Mia Green': 'Woman, 35, White, Independent',
-            'Xavier Littlebear': 'Nonbinary, 40, Native American/Alaska Native, Liberal',
-            'Lily Wong': 'Woman, 52, Asian, Conservative',
-            'Alejandro Ramirez': 'Man, 33, Latino, Moderate',
-            'Hannah Smith': 'Woman, 18, White, Independent',
-            'Malikah Hussein': 'Woman, 40, Black, Liberal',
-            'Ethan Kim': 'Man, 62, Asian, Conservative',
-            'Carlos Santos': 'Man, 50, Latino, Moderate'}
-DEMO_KEYS = ['gender', 'age', 'ethnicity', 'politics']
+DEFAULT_TEMPERATURE = 0.8
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 def draw_and_save_network_plot(G, save_prefix):
     """
     Draw network, save figure.
     """
-    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=0))
+    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=0, k=2*1/np.sqrt(len(G.nodes()))))
     plt.axis("off")  # turn off axis
     axis = plt.gca()
     axis.set_xlim([1.1*x for x in axis.get_xlim()])  # add padding so that node labels aren't cut off
@@ -42,6 +30,16 @@ def save_network(G, save_prefix):
     graph_path = os.path.join(PATH_TO_TEXT_FILES, f'{save_prefix}.adj')
     print('Saving adjlist in ', graph_path)
     nx.write_adjlist(G, graph_path)
+
+def extract_gpt_output(response):
+    """
+    Extract output message from GPT, check for finish reason.
+    """
+    response = response.choices[0]
+    finish_reason = response['finish_reason']
+    if finish_reason != 'stop':
+        raise Exception(f'Response stopped for reason {finish_reason}')
+    return response['message']['content']
 
 def get_node_from_string(s):
     """
