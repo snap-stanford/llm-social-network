4082fd9e56c9912c063687d787d4505e18650a65
Serina Chang <serinac@stanford.edu>
2024-08-24 11:05:51 -0400

    added functions to analyze networks, changed homophily to same-group instead of cross-group

--- analyze_networks.py
+++ analyze_networks.py
@@ -1,3 +1,4 @@
+from collections import Counter 
 import networkx as nx
 import os
 import matplotlib.pyplot as plt
@@ -87,52 +88,84 @@ def get_edge_summary(list_of_G, save_name):
 
     plotting.plot_props(props, edges, save_name)
 
+def compute_exp_cross_from_group_counts(group_counts):
+    """
+    Compute expected proportion of edges that are cross-relations, given
+    number of nodes that are in each group.
+    This matches the method below that uses the complete graph.
+    """
+    groups = list(group_counts.keys())
+    cr_total = 0
+    for i1, g1 in enumerate(groups[:-1]):
+        for g2 in groups[i1+1:]:
+            cr_total += group_counts[g1] * group_counts[g2]
+    num_nodes = np.sum(list(group_counts.values()))
+    total_num_edges = num_nodes * (num_nodes-1) / 2
+    return cr_total / total_num_edges 
 
 def compute_cross_proportions(G, personas, demo_keys, ratio=True):
     """
-    Compute homophily as proportion of edges that are cross-relations,
-    per demographic variable.
+    Compute proportion of edges that are cross-relations, per demographic variable.
     If ratio is true, divide by expected proportions.
     """
-    props = _compute_cross_proportions(G, personas, demo_keys)
-    if ratio:
-        # get expected proportions of cross-relations from complete graph
-        complete = nx.complete_graph(G.nodes(), create_using=nx.DiGraph())
-        exp_props = _compute_cross_proportions(complete, personas, demo_keys)
-        props /= exp_props
-    return props
+    observed = _compute_cross_proportions(G, personas, demo_keys)
+    if not ratio:
+        return observed 
+    complete = nx.complete_graph(G.nodes())
+    expected = _compute_cross_proportions(complete, personas, demo_keys)
+    return observed / expected
 
 def _compute_cross_proportions(G, personas, demo_keys):
     """
     Helper function to compute the proportion of edges in the graph that are 
     cross-relations, per demographic variable.
-    """
+    """ 
     # count cross-relationships in graph
     crs = np.zeros(len(demo_keys))
     for source, target in G.edges():
-        try:
-            demo1 = personas[source]
-            demo2 = personas[target]
-        except:
-            print('Error with source:', source, 'target:', target)
-            print('Personas:', personas)
-            quit()
+        demo1 = personas[source]
+        demo2 = personas[target]
         for ind, d in enumerate(demo_keys):
             if d == 'age':  # take absolute difference for age
-                crs[ind] += abs(int(demo1[d]) - int(demo2[d]))
+                diff = abs(int(demo1[d]) - int(demo2[d]))
             else:
-                if demo1[d] != demo2[d]:
-                    crs[ind] += 1
-    props = crs / len(G.edges())  # get proportion of edges
+                diff = int(demo1[d] != demo2[d])  # 1 if they are different, 0 otherwise
+            crs[ind] += diff
+    # get proportion of edges that are cross-relations or average difference in age
+    props = crs / len(G.edges())  
     return props
 
-def compute_cross_proportions_within_demo(G, personas, demo_keys, demo):
+def compute_same_proportions(G, personas, demo_keys, ratio=True):
     """
-    TODO: return a matrix of type1-type2 ratios (eg, Man-Man, Man-Woman, Man-Nonbinary, etc).
-    These should still be ratios, ie, actual proportions divided by expected proportions.
+    Compute proportion of edges that are same-group relations, per demographic variable.
+    If ratio is true, divide by expected proportions.
     """
-    pass
+    observed = _compute_same_proportions(G, personas, demo_keys)
+    if not ratio:
+        return observed 
+    complete = nx.complete_graph(G.nodes())
+    expected = _compute_same_proportions(complete, personas, demo_keys)
+    return observed / expected
 
+def _compute_same_proportions(G, personas, demo_keys):
+    """
+    Helper function to compute the proportion of edges in the graph that are 
+    same relations, per demographic variable.
+    """ 
+    # count same-relationships in graph
+    same_counts = np.zeros(len(demo_keys))
+    for source, target in G.edges():
+        demo1 = personas[source]
+        demo2 = personas[target]
+        for ind, d in enumerate(demo_keys):
+            if d == 'age':  # check whether age is within 10
+                same = int(abs(int(demo1[d]) - int(demo2[d])) <= 10)
+            else:
+                same = int(demo1[d] == demo2[d])
+            same_counts[ind] += same
+    # get proportion of edges that are same relation
+    props = same_counts / len(G.edges())  
+    return props
 
 def summarize_network_metrics(list_of_G, personas, demo_keys, save_name, demos=True):
 
@@ -143,7 +176,7 @@ def summarize_network_metrics(list_of_G, personas, demo_keys, save_name, demos=T
     if demos:
         homophily_metrics_df = pd.DataFrame({'graph_nr':[], 'demo':[], '_metric_value':[], 'save_name':[]})
         for graph_nr, G in enumerate(list_of_G):
-            homophily_metrics = list(compute_cross_proportions(G, personas, demo_keys))
+            homophily_metrics = list(compute_same_proportions(G, personas, demo_keys))
             # concat with series
             homophily_metrics_df = pd.concat([homophily_metrics_df,
                                               pd.DataFrame({'graph_nr':graph_nr, 'demo':demo_keys,
@@ -157,15 +190,18 @@ def summarize_network_metrics(list_of_G, personas, demo_keys, save_name, demos=T
     ### ---------------------------------- scalar network metrics ---------------------------------- ###
     network_metrics_df = pd.DataFrame({'graph_nr':[], 'metric_name':[], '_metric_value':[], 'save_name':[]})
 
-    network_metrics = ['density', 'avg_clustering_coef', 'prop_nodes_lcc', 'radius', 'diameter']
-    network_func = [nx.density, nx.average_clustering, prop_nodes_in_giant_component, nx.radius, nx.diameter]
+    network_metrics = ['density', 'avg_clustering_coef', 'prop_nodes_lcc', 'radius', 'diameter', 'avg_shortest_path', 'modularity']  # 'assortativity', 
+    network_func = [nx.density, nx.average_clustering, prop_nodes_in_giant_component, nx.radius, nx.diameter, nx.average_shortest_path_length, nx.community.modularity]
     for graph_nr, G in enumerate(list_of_G):
 
         for metric_name, f in zip(network_metrics, network_func):
-            if metric_name in ['radius', 'diameter']:
+            if metric_name in ['radius', 'diameter', 'avg_shortest_path']:
+                # use LCC for connectivity measures
                 largest_cc = sorted(nx.connected_components(G.to_undirected()), key=len, reverse=True)[0]
-                _metric_value = f(G.subgraph(largest_cc).to_undirected()) / len(G.nodes())
-                ### PLEASE NOTE LCC HERE
+                _metric_value = f(G.subgraph(largest_cc).to_undirected()) / np.log(len(largest_cc))
+            elif metric_name == 'modularity':
+                comms = nx.community.louvain_communities(G.to_undirected())  # get communities with Louvain
+                _metric_value = f(G.to_undirected(), comms)
             else:
                 _metric_value = f(G.to_undirected())
 
@@ -190,6 +226,99 @@ def summarize_network_metrics(list_of_G, personas, demo_keys, save_name, demos=T
     fn = f'{save_name}/network_metrics.csv'
     network_metrics_df.to_csv(os.path.join(PATH_TO_STATS_FILES, fn), index=False)
     print("Saved network metrics to " + fn)
+
+
+def compute_pairwise_ratios(G, personas, demo, cutoff=1):
+    """
+    Compute matrix where m[a, b] represents the observed number of edges between
+    nodes in group a and b divided by expected number of such edges.
+    """
+    vals = [personas[k][demo] for k in personas]
+    groups = [g for g,c in Counter(vals).most_common() if c >= cutoff]
+    obs_mat = _compute_pairwise_props(G, personas, demo, groups)
+    exp_mat = _compute_pairwise_props(nx.complete_graph(G.nodes()), personas, demo, groups)
+    ratio = obs_mat / exp_mat
+    return groups, ratio
+
+
+def _compute_pairwise_props(G, personas, demo, groups):
+    assert type(groups) == list 
+    mat = np.zeros((len(groups), len(groups)))
+    for u,v in G.to_undirected().edges():
+        if (personas[u][demo] in groups) and (personas[v][demo] in groups):
+            g1 = groups.index(personas[u][demo])
+            g2 = groups.index(personas[v][demo])
+            mat[g1][g2] += 1
+            mat[g2][g1] += 1
+    return mat / np.sum(mat)
+    
+
+def compute_isolation_index(G, personas):
+    """
+    Compute political isolation index, following Halberstam and Knight (2016).
+    """
+    nodes = list(G.nodes())
+    A = nx.adjacency_matrix(G, nodelist=nodes).todense()
+    politics = np.array([personas[n]['political affiliation'] for n in nodes])
+    assert A.shape == (len(politics), len(politics))
+
+    # compute share conservative
+    num_neighbors_c = A @ (politics == 'Republican').astype(int)
+    num_neighbors_l = A @ (politics == 'Democrat').astype(int)
+    share_conservative = num_neighbors_c / (num_neighbors_c + num_neighbors_l)
+    
+    # compute conservative exposure
+    degree = np.sum(A, axis=0)
+    conservative_exposure = (A @ share_conservative) / degree
+
+    # compute isolation
+    avg_exposure_c = np.mean(conservative_exposure[politics == 'Republican'])
+    avg_exposure_l = np.mean(conservative_exposure[politics == 'Democrat'])
+    isolation = avg_exposure_c-avg_exposure_l
+    return isolation, avg_exposure_c, avg_exposure_l
+
+def compute_polarization(G, personas):
+    """
+    Compute polarization, following Garimella and Weber (2017).
+    """
+    nodes = list(G.nodes())
+    A = nx.adjacency_matrix(G, nodelist=nodes).todense()
+    politics = np.array([personas[n]['political affiliation'] for n in nodes])
+    assert A.shape == (len(politics), len(politics))
+
+    alpha = np.ones(len(politics))
+    beta = np.ones(len(politics))
+    alpha += A @ (politics == 'Democrat').astype(int)
+    beta += A @ (politics == 'Republican').astype(int)
+    lean = alpha / (alpha + beta)
+    pol = 2 * np.abs(0.5 - lean)
+    return pol 
+
+def plot_expected_vs_observed_age_gaps(list_of_G, personas):
+    """
+    """
+    obs_gaps = []
+    for G in list_of_G:
+        for (u,v) in G.edges():
+            gap = np.abs(personas[u]['age'] - personas[v]['age'])
+            obs_gaps.append(gap)
+    
+    exp_gaps = []
+    complete = nx.complete_graph(list_of_G[0].nodes())
+    for (u,v) in complete.edges():
+        gap = np.abs(personas[u]['age'] - personas[v]['age'])
+        exp_gaps.append(gap)
+
+    bins = np.arange(0, 101, 5)
+    plt.figure(figsize=(6,4))
+    plt.hist(exp_gaps, color='tab:blue', label='expected', density=True, bins=bins)
+    plt.hist(obs_gaps, color='tab:orange', alpha=0.5, density=True, label='observed', bins=bins)
+    plt.xlabel('Age gap btwn friends', fontsize=16)
+    plt.grid(alpha=0.2)
+    # ymin, ymax = plt.ylim()
+    # plt.vlines([np.mean(exp_gaps)], ymin, ymax, color='tab:blue', label=f'exp mean={np.mean(exp_gaps):0.3f}')
+    # plt.vlines([np.mean(obs_gaps)], ymin, ymax, color='tab:orange', label=f'obs mean={np.mean(obs_gaps):0.3f}')
+    plt.legend()
 
 
 def parse():
